4ca7b07c867c5fb0506c982ec18868098dbe6d97
july-12 <seventanyb@gmail.com>
2022-11-05 22:41:09 +0800

    next: config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   assetPrefix: isProd ? '/nextjs-blog-github-pages/' : '',
+  basePath: '/nextjs-blog-github-pages',
   images: {
     unoptimized: true,
   },
